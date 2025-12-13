8d620ffe57a3f02c313c616acee4f7a4353b2bc0
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-13 17:19:35 +0530

    Created a card

---
+++ 02_hover_and_exit_animations/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/card";
+
+export default function Home() {
+  return (
+   <div className="min-h-screen max-w-4xl flex items-center justify-center">
+   <Card />
+   </div>
+  );
+}
