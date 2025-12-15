fd3ad750ecc91757dd6d7123f0559f16ba757e7e
Shubham Nagar <shubhamnagar234@gmail.com>
2025-12-15 18:37:29 +0530

    Applied hover and exit animations to card

--- 02_hover_and_exit_animations/src/app/page.tsx
+++ 02_hover_and_exit_animations/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/card";
 
 export default function Home() {
   return (
-   <div className="min-h-screen max-w-4xl flex items-center justify-center">
+   <div className="min-h-screen flex items-center justify-center">
    <Card />
    </div>
   );
--- 02_hover_and_exit_animations/src/components/card.tsx
+++ 02_hover_and_exit_animations/src/components/card.tsx
@@ -1,32 +1,148 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import { motion } from "motion/react";
+import {
+  X,
+  Zap,
+  MessageCircle,
+  Clock,
+  RotateCcw,
+  Box,
+  Plus,
+} from "lucide-react";
+import { useState } from "react";
 
 export const Card = () => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div
-      className={cn(
-        "w-72 min-h-104 h-104 rounded-xl",
-        "shadow-[0px_1px_1px_rgba(0,0,0,00.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]",
-        "p-6, flex flex-col items-center"
+    <>
+      {open && (
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.98,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            filter: "blur(0px)",
+          }}
+          exit={{
+            opacity: 0,
+            scale: 0.98,
+            filter: "blur(10px)",
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+          className={cn(
+            "w-80 min-h-120 h-136 rounded-xl",
+            "shadow-[0px_1px_1px_rgba(0,0,0,00.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]",
+            "p-6 flex flex-col items-center"
+          )}
+        >
+          <h2 className="font-bold text-[16px]">Aceternity UI Components</h2>
+          <p className="text-neutral-500 mt-2 text-[14px]">
+            A collection of beautiful UI components, let&apos;s get on with it.
+          </p>
+          <div className="flex items-center justify-center">
+            <button
+              onClick={() => setOpen(false)}
+              className="flex items-center gap-1 text-[10px] mt-4 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md px-2 py-1"
+            >
+              <Zap className="h-4 w-4" />
+              <span className="text-[14px]">Acerternity</span>
+              <X className="h-3 w-3 text-neutral-400" />
+            </button>
+          </div>
+          <div className="bg-gray-100 flex-1 mt-4 rounded-lg border border-dashed border-neutral-200">
+            <motion.div
+              initial={{
+                opacity: 0,
+                scale: 0.98,
+                filter: "blur(10px)",
+              }}
+              whileHover={{
+                opacity: 1,
+                scale: 1.05,
+                filter: "blur(0px)",
+              }}
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+              className="h-full w-full bg-white border border-neutral-200 rounded-lg divide-y divide-neutral-200"
+            >
+              <div className="flex gap-2 p-4">
+                <div className="h-7 w-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                  <MessageCircle className="h-4 w-4 text-neutral-600" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-[12px] font-bold text-neutral-600">
+                    Acerternity UI Components
+                  </p>
+                  <p className="text-neutral-400 text-[12px] mt-1">
+                    A collection of UI components
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 p-4">
+                <div className="h-7 w-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                  <Clock className="h-4 w-4 text-neutral-600" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-[12px] font-bold text-neutral-600">
+                    24 hours turnaround
+                  </p>
+                  <p className="text-neutral-400 text-[12px] mt-1">
+                    Super fast delivery at warp speed
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 p-4">
+                <div className="h-7 w-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                  <RotateCcw className="h-4 w-4 text-neutral-600" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-[12px] font-bold text-neutral-600">
+                    360 days all around
+                  </p>
+                  <p className="text-neutral-400 text-[12px] mt-1">
+                    We are help you 24/7
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 p-4">
+                <div className="h-7 w-7 shrink-0 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                  <Box className="h-4 w-4 text-neutral-600" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-[12px] font-bold text-neutral-600">
+                    Some other components
+                  </p>
+                  <p className="text-neutral-400 text-[12px] mt-1">
+                    Here goes another subtitle
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 p-4 items-center justify-center">
+                <div className="h-4 w-4 shrink-0 bg-linear-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                  <Plus className="h-3 w-3 text-neutral-600" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-neutral-400 text-[12px] mt-1">
+                    Create Project
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        </motion.div>
       )}
-    >
-      <h2 className="font-bold text-[10px]">Aceternity UI Components</h2>
-      <p className="text-neutral-500 mt-2 text-[10px]">
-        A collection of beautiful UI components, let&apos;s get on with it.
-      </p>
-      <div className="flex items-center justify-center">
-        <button className="flex items-center gap-1 text-[10px] mt-4 shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] rounded-md px-2 py-1">
-            <Image 
-            src="/vercel.svg"
-            alt="vercel"
-            width={50}
-            height={50}
-            className="h-4 w-4"
-            />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
